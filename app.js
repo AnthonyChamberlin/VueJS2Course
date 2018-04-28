4e6ebff2e84d6205897334e921176dcd34597c1a
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-04-28 09:10:23 +0100

    events

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
 new Vue({
     el:'#vue-app',
     data: {
-        name: 'Ant',
-        job: 'Developer',
-        website: 'https://antchamberlin.me',
-        websiteTag: '<a href="https://antchamberlin.me">My Portfolio</a>'
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-
+        add: function(inc) {
+            this.age += inc;
+        },
+        subtract: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
     }
 });
