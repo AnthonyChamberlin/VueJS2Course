4e2392665f7670b12e3f56216342fa6169d8605e
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-04-28 23:17:58 +0100

    punchbag-game

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
+document.body.style.backgroundColor = "#2e2e2e";
 // Create new Vue instance to grab html element
 new Vue({
     el:'#vue-app',
     data: {
-      name: 'Ant',
-      characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-      ninjas: [
-        { 
-            name: 'Ryu',
-            age: 35
-        },
-        { 
-            name: 'Yoshi',
-            age: 35
-        },
-        { 
-            name: 'Ken',
-            age: 23
-        },
-      ]
+      health: 100,
+      ended: false
     },
     methods: {
-        
+       punch: function() {
+           this.health -= 10;
+           if ( this.health <= 0) {
+               this.ended = true;
+           }
+       },
+       restart: function() {
+           this.health = 100;
+           this.ended = false;
+       } 
     },
     computed: {
     
